e4ba714445bbc2abbb97c09cde55c1915260b9cf
Norman-Brooks <nbprogramming@outlook.com>
2024-12-26 11:44:42 -0800

    commit

---
+++ Simple_Recipes/javascript/javascript.js
@@ -0,0 +1,19 @@
+//RECIPE POP UP MODAL SECTION
+
+//SETS UP THE BUTTON THAT WILL OPEN THE RECIPE MODEL
+var btns = document.querySelectorAll("input.modal-button");
+
+//DEFINES ALL MODALS FOR EACH RECIPE
+var modals = document.querySelectorAll(".recipe-modal");
+
+// GET THE <SPAN> ELEMENT THAT CLOSES THE MODAL
+var closeBtn = document.getElementsByClassName("close-btn");
+
+//When the user clicks the recipe button, open the modal
+for (var i = 0; i < btns.length; i++) {
+    btns[i].onclick = function (event) {
+            modal = document.querySelector(event.target.getAttribute
+            ("href")); 
+            modal.style.display = "block";
+    }
+}
