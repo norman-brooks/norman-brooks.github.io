c95996e681b53c5974fcabca763655131d2cc765
Norman-Brooks <nbprogramming@outlook.com>
2024-12-26 16:55:50 -0800

    Commit

--- Simple_Recipes/javascript/javascript.js
+++ Simple_Recipes/javascript/javascript.js
@@ -12,8 +12,19 @@ var closeBtn = document.getElementsByClassName("close-btn");
 // When the user clicks the recipe button, open the modal
 for (var i = 0; i < btns.length; i++) {
     btns[i].onclick = function (event) {
-            modal = document.querySelector(event.target.getAttribute
-            ("href")); 
+            let modal = document.querySelector
+            (event.target.getAttribute ("href")); 
             modal.style.display = "block";
+    }
+}
+
+//When the user clicks on <span> (x) close the modal
+for (var i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].onclick = function () {
+     for (var index in modals) {
+        if (modals[index].style) {
+            modals[index].style.display = "none";
+        }
+     }  
     }
 }
